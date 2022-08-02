111d5c4a7171ed1847b61f0cf3431848ac876c5e
Valdemar Erk <valdemar@erk.dev>
2022-08-02 13:43:38 +0200

    document what the generated code does

--- build/util.rs
+++ build/util.rs
@@ -10,6 +10,12 @@ pub fn to_module_name<P: Into<PathBuf>>(file_name: P) -> String {
         .replace(|c: char| !c.is_alphanumeric(), "_") // remove non alphanum
 }
 
+/// Removes the trailing zeroes in the payload
+///
+/// # Note:
+///
+/// There must always be at least one remaining byte even if it is a
+/// zero byte.
 pub fn q_remove_trailing_zeroes(buf: quote::Ident) -> quote::Tokens {
     quote::quote!{
         while let Some(&0) = #buf.last() {
